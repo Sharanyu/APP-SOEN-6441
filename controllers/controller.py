7ce11ca5bdd9274365956d5430df1391b7a45374
sanv00 <mailpsss@gmail.com>
2022-11-10 11:14:18 -0500

    Updates on Controller and Views
    1)Merged Routes to Controllers
    2)Added new query function on controllers and respective html pages in views
    3)Added more columns in views for better query response
    
    Co-Authored-By: Sharanyu <41756221+Sharanyu@users.noreply.github.com>

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,5 +1,5 @@
 from flask import *
-from controllers.UserController import DBController
+from models.models import DBController
 
 
 app = Flask(__name__,template_folder="../templates/")
@@ -8,11 +8,24 @@ obj=DBController()
 
 @app.route("/")
 def index():
-    return render_template("index.html",)
+    return render_template("index.html", )
 
 @app.route("/find_index")
 def find():
     return render_template("find.html")
+
+@app.route("/find_emp_by_borough_index")
+def filter():
+    return render_template("borough.html")
+
+
+@app.route("/borough_filter_borough", methods=["POST", "GET"])
+def filter_borough():
+    borough = request.form["borough"]
+    rows= obj.view_emp_from(borough)
+    #rows=UserController.find_emp(id)
+    return render_template("borough_success.html", rows=rows)
+
 
 @app.route("/view_index")
 def view():
