5253f0f50b47b675d3126e0ad9dd517fbf72e4d6
Sharanyu <41756221+Sharanyu@users.noreply.github.com>
2022-11-11 12:33:30 -0500

    Refactoring controller and views
    Refactored the controller.py and view pages
    
    Co-Authored-By: sanv00 <45181379+sanv00@users.noreply.github.com>

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,18 +1,22 @@
 from flask import *
 from models.models import DBController
 
+app = Flask(__name__, template_folder="../templates/")
 
-app = Flask(__name__,template_folder="../templates/")
+obj = DBController()
 
-obj=DBController()
 
 @app.route("/")
 def index():
-    return render_template("index.html", )
+    return render_template(
+        "index.html",
+    )
+
 
 @app.route("/find_index")
 def find():
     return render_template("find.html")
+
 
 @app.route("/find_emp_by_borough_index")
 def filter():
@@ -22,22 +26,23 @@ def filter():
 @app.route("/borough_filter_borough", methods=["POST", "GET"])
 def filter_borough():
     borough = request.form["borough"]
-    rows= obj.view_emp_from(borough)
-    #rows=UserController.find_emp(id)
+    rows = obj.view_emp_from(borough)
+    # rows=UserController.find_emp(id)
     return render_template("borough_success.html", rows=rows)
 
 
 @app.route("/view_index")
 def view():
-    rows= obj.view_emp()
-    #emp_data=obj.find("employee")
+    rows = obj.view_emp()
+    # emp_data=obj.find("employee")
 
-    #rows=UserController.view_emp()
+    # rows=UserController.view_emp()
     return render_template("view.html", rows=rows)
+
 
 @app.route("/find_findID", methods=["POST", "GET"])
 def find_id():
     id = request.form["id"]
-    rows= obj.find_emp(id)
-    #rows=UserController.find_emp(id)
+    rows = obj.find_emp(id)
+    # rows=UserController.find_emp(id)
     return render_template("find_success.html", rows=rows)
