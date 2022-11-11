57ea48694a6a7d3851c637be42f981d1207e4537
Sharanyu <41756221+Sharanyu@users.noreply.github.com>
2022-11-11 10:42:24 -0500

    updated employee_details model and models.py
    Added an extra function in models to show top 5 highest earner every year

--- models/employeedetails.py
+++ models/employeedetails.py
@@ -1,19 +1,22 @@
 class employeedetails:
     def _int_(self):
-        self.employee_id =None,
-        self.title_description=None,
-        self.work_location_borough=None,
-        self.fiscal_year=None, 
-        self.pay_basis=None,
-        self.base_salary_USD=None, 
-        self.work_hours=None,
-        self.gross_salary_USD=None, 
-        self.overtime_hours=None, 
-        self.overtime_commission_USD=None,
-        self.other_pay_USD=None
-    def set_location(self,location):
+        self.employee_id = None
+        self.title_description = None
+        self.work_location_borough =None
+        self.fiscal_year = None
+        self.pay_basis = None
+        self.base_salary_USD =None
+        self.work_hours = None
+        self.gross_salary_USD = None
+        self.overtime_hours =None
+        self.overtime_commission_USD = None
+        self.other_pay_USD =None
+    def set_location(self, location):
         self.work_location_borough = location
     def get_location(self):
         return self.work_location_borough
-    
-empdetails=employeedetails()
+    def set_fiscal_year(self, fiscal_year):
+        self.fiscal_year = fiscal_year
+    def get_fiscal_year(self):
+        return self.fiscal_year
+empdetails = employeedetails()
--- models/models.py
+++ models/models.py
@@ -28,12 +28,8 @@ class DBController():
         self.cur = self.con.cursor()
         emp.set_emp_id(id)
         form_query = emp.get_emp_id()
-        print(
-            'SELECT employee.employee_id,employee_name,agency_name,fiscal_year,work_location_borough,title_description,work_hours,gross_salary_USD FROM employee JOIN employee_details ON employee.employee_id = employee_details.employee_id JOIN payroll_reference ON employee.employee_id = payroll_reference.employee_id JOIN agency ON payroll_reference.payroll_number = agency.payroll_number WHERE employee.employee_id ="{0}"'.format(
-                form_query))
-        self.cur.execute(
-            'SELECT employee.employee_id,employee_name,agency_name,fiscal_year,work_location_borough,title_description,work_hours,gross_salary_USD FROM employee JOIN employee_details ON employee.employee_id = employee_details.employee_id JOIN payroll_reference ON employee.employee_id = payroll_reference.employee_id JOIN agency ON payroll_reference.payroll_number = agency.payroll_number WHERE employee.employee_id ="{0}"'.format(
-                form_query))
+        print('SELECT employee.employee_id,employee_name,agency_name,fiscal_year,work_location_borough,title_description,work_hours,gross_salary_USD FROM employee JOIN employee_details ON employee.employee_id = employee_details.employee_id JOIN payroll_reference ON employee.employee_id = payroll_reference.employee_id JOIN agency ON payroll_reference.payroll_number = agency.payroll_number WHERE employee.employee_id ="{0}"'.format(form_query))
+        self.cur.execute('SELECT employee.employee_id,employee_name,agency_name,fiscal_year,work_location_borough,title_description,work_hours,gross_salary_USD FROM employee JOIN employee_details ON employee.employee_id = employee_details.employee_id JOIN payroll_reference ON employee.employee_id = payroll_reference.employee_id JOIN agency ON payroll_reference.payroll_number = agency.payroll_number WHERE employee.employee_id ="{0}"'.format(form_query))
         rows = self.cur.fetchall()
         return rows
 
@@ -41,8 +37,7 @@ class DBController():
         self.con = sqlite3.connect("nyc.db")
         self.con.row_factory = sqlite3.Row
         self.cur = self.con.cursor()
-        self.cur.execute(
-            "select employee.employee_id,employee.employee_name,fiscal_year,work_location_borough,gross_salary_USD from employee INNER JOIN employee_details ON employee.employee_id=employee_details.employee_id limit 5")
+        self.cur.execute("select employee.employee_id,employee.employee_name,fiscal_year,work_location_borough,gross_salary_USD from employee INNER JOIN employee_details ON employee.employee_id=employee_details.employee_id limit 5")
         rows = self.cur.fetchall()
         return rows
 
@@ -52,11 +47,18 @@ class DBController():
         self.cur = self.con.cursor()
         empdetails.set_location(location)
         form_query = empdetails.get_location()
-        print(
-            'select employee.employee_id,employee_name from employee INNER JOIN employee_details on employee.employee_id=employee_details.employee_id WHERE work_location_borough="{0}"'.format(
-                form_query))
-        self.cur.execute(
-            'select employee.employee_id,employee_name,fiscal_year from employee join employee_details on employee.employee_id=employee_details.employee_id where work_location_borough="{0}"'.format(
-                form_query))
+        print('select employee.employee_id,employee_name from employee INNER JOIN employee_details on employee.employee_id=employee_details.employee_id WHERE work_location_borough="{0}"'.format(form_query))
+        self.cur.execute('select employee.employee_id,employee_name,fiscal_year from employee join employee_details on employee.employee_id=employee_details.employee_id where work_location_borough="{0}"'.format(form_query))
+        rows = self.cur.fetchall()
+        return rows
+    
+    def top_5_earners(self,fiscal_year):
+        self.con = sqlite3.connect("nyc.db")
+        self.con.row_factory = sqlite3.Row
+        self.cur = self.con.cursor()
+        empdetails.set_fiscal_year(fiscal_year)
+        form_query = empdetails.get_fiscal_year()
+        print('select employee_id, employee_name, gross_salary_USD from employee join employee_details on employee.employee_id = employee_details.employee_id where fiscal_year = {0} order by gross_salary_USD desc limit 5'.format(form_query))
+        self.cur.execute('select employee_id, employee_name, gross_salary_USD from employee join employee_details on employee.employee_id = employee_details.employee_id where fiscal_year = {0} order by gross_salary_USD desc limit 5'.format(form_query))
         rows = self.cur.fetchall()
         return rows
